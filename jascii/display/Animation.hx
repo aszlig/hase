dfbd3da9e7a7a488fc33e86cbb48b532c9084ee2
aszlig <aszlig@redmoonstudios.org>
2013-03-20 03:25:21 +0100

    Animation: Allow loading animations from files.
    This is implemented as a macro so the contents of the animation file will be
    compiled into an array declaration.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- jascii/display/Animation.hx
+++ jascii/display/Animation.hx
@@ -1,5 +1,7 @@
 package jascii.display;
 
+import haxe.macro.Expr;
+
 class Animation extends Image
 {
     private var frames:Array<Array<String>>;
@@ -8,14 +10,71 @@ class Animation extends Image
     private var td:Int;
     public var factor:Int;
 
-    public function new()
+    public function new(?frames = null)
     {
         super();
-        this.frames = new Array();
+
+        if (frames == null)
+            frames = new Array();
+
+        this.frames = frames;
         this.current = 0;
 
         this.td = 0;
         this.factor = 1;
+    }
+
+    @:macro public static function from_file(path:String):Expr
+    {
+        var lines:Array<String> = sys.io.File.getContent(path).split("\n");
+
+        var frames:Array<Array<String>> = new Array();
+
+        for (line in lines) {
+            var delim:Int = line.indexOf(" ");
+            var frame_id:Null<Int> = Std.parseInt(line.substr(0, delim));
+            if (frame_id == null)
+                continue;
+
+            var content:String = line.substr(delim);
+
+            if (frames[frame_id] == null)
+                frames.insert(frame_id, [content]);
+            else
+                frames[frame_id].push(content);
+        }
+
+        var expr_array:Array<Expr> = new Array();
+
+        for (frame in frames) {
+            var row_array:Array<Expr> = new Array();
+
+            for (row in frame)
+                row_array.push({
+                    expr: EConst(CString(row)),
+                    pos: haxe.macro.Context.currentPos()
+                });
+
+
+            expr_array.push({
+                expr: EArrayDecl(row_array),
+                pos: haxe.macro.Context.currentPos()
+            });
+        }
+
+        var frames_expr:Expr = {
+            expr: EArrayDecl(expr_array),
+            pos: haxe.macro.Context.currentPos()
+        };
+
+        return {
+            expr: ENew({
+                name: "Animation",
+                pack: ["jascii", "display"],
+                params: []
+            }, [frames_expr]),
+            pos: haxe.macro.Context.currentPos()
+        };
     }
 
     public inline function add_frame(frame:Array<String>):Array<String>
