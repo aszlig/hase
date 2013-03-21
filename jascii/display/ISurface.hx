3be35e9bd33853fa9f26e8fd38c1ce60d5118cec
aszlig <aszlig@redmoonstudios.org>
2013-03-21 19:00:01 +0100

    Refactor surfaces to be decoupled of TermCanvas.
    This makes it easier to create different implementations for the underlying
    drawing engine and should help in porting to different platforms.
    
    So, now we have a new class Surface, which wraps the calls to the canvas
    implementation (currently only draw_char).
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- jascii/display/ISurface.hx
+++ jascii/display/ISurface.hx
@@ -3,4 +3,7 @@ package jascii.display;
 interface ISurface
 {
     public function draw_char(x:Int, y:Int, char:Int):Void;
+    // XXX: These should be handled by events (such as resize).
+    public var width:Int;
+    public var height:Int;
 }
---
+++ jascii/display/Surface.hx
@@ -0,0 +1,21 @@
+package jascii.display;
+
+class Surface extends ObjectContainer
+{
+    private var provider:ISurface;
+
+    public function new(provider:ISurface)
+    {
+        super();
+        this.provider = provider;
+        this.width = provider.width;
+        this.height = provider.height;
+        this.autoresize = false;
+    }
+
+    @:keep public inline function draw_char(x:Int, y:Int, ordinal:Int):Void
+    {
+        if (x <= this.width && y <= this.height)
+            this.provider.draw_char(x, y, ordinal);
+    }
+}
