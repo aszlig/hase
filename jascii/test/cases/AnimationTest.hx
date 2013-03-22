e8e08ebbcbbffa35eb9f183f7153e95bd9ac6bcb
aszlig <aszlig@redmoonstudios.org>
2013-03-22 06:23:14 +0100

    Add overlay intersaction tests.
    This adds a test case for single frame animation and for multiple frame
    animation, where the sprites overlap by intersection.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- jascii/test/cases/AnimationTest.hx
+++ jascii/test/cases/AnimationTest.hx
@@ -212,4 +212,53 @@ class AnimationTest extends jascii.test.SurfaceTestCase
             ], 2, 0, 17, 7
         );
     }
+
+    public function test_overlay_intersect()
+    {
+        var circle:jascii.display.Animation = this.create_animation([
+            [ "   _._   "
+            , " .'   `. "
+            , " :     ; "
+            , "  `-.-'  "
+            , "         "
+            ],
+            [ "   .-.   "
+            , " ,'   `. "
+            , " :     ; "
+            , " `.   ,' "
+            , "   `-'   "
+            ]
+        ]);
+
+        var rect:jascii.display.Animation = this.create_animation([
+            [ ",-------."
+            , "|       |"
+            , "|       |"
+            , "|       |"
+            , "`-------'"
+            ]
+        ]);
+
+        rect.add_child(circle);
+        this.root.add_child(rect);
+
+        rect.x = 4;
+        rect.y = 1;
+
+        circle.x = -4;
+        circle.y = -1;
+
+        this.root.update();
+        this.root.update();
+
+        this.assert_area(
+            [ "   .-.       "
+            , " ,' ,-`.----."
+            , " :  |  ;    |"
+            , " `. | ,'    |"
+            , "   `-'      |"
+            , "    `-------'"
+            ], 0, 0, 13, 6
+        );
+    }
 }
--- jascii/test/cases/SpriteTest.hx
+++ jascii/test/cases/SpriteTest.hx
@@ -128,4 +128,46 @@ class SpriteTest extends jascii.test.SurfaceTestCase
             ], 0, 0, 9, 6
         );
     }
+
+    public function test_overlay_intersect()
+    {
+        var circle:jascii.display.Animation = this.create_animation([
+            [ "   _._   "
+            , " .'   `. "
+            , " :     ; "
+            , "  `-.-'  "
+            , "         "
+            ]
+        ]);
+
+        var rect:jascii.display.Animation = this.create_animation([
+            [ ",-------."
+            , "|       |"
+            , "|       |"
+            , "|       |"
+            , "`-------'"
+            ]
+        ]);
+
+        rect.add_child(circle);
+        this.root.add_child(rect);
+
+        rect.x = 4;
+        rect.y = 1;
+
+        circle.x = -4;
+        circle.y = -1;
+
+        this.root.update();
+
+        this.assert_area(
+            [ "   _._       "
+            , " .' ,-`.----."
+            , " :  |  ;    |"
+            , "  `-.-'     |"
+            , "    |       |"
+            , "    `-------'"
+            ], 0, 0, 13, 6
+        );
+    }
 }
