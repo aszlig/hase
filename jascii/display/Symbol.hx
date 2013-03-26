aszlig/hase

commit f323bfb511569f87ce6a54fe7110789c3ebe3328

Add display.Symbol class for special attributes.
This class is mainly for merging the character ordinal with the additional
attributes, for now the background and foreground colors.

Obviously, this will cause a performance regression, so we need to optimize this
later using macros and better inlining.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

jascii/display/Symbol.hx
@@ -1,0 +1,25 @@
+package jascii.display;
+
+class Symbol
+{
+    public var ordinal(default, null):Int;
+    public var fgcolor(default, null):Int;
+    public var bgcolor(default, null):Int;
+
+    public inline function new(ord:Int, fg:Int = 0, bg:Int = 0)
+    {
+        this.ordinal = ord;
+        this.fgcolor = fg;
+        this.bgcolor = bg;
+    }
+
+    public inline function get_hash():Int
+    {
+        return this.ordinal << 16 | this.fgcolor << 8 | this.bgcolor;
+    }
+
+    public inline function is_alpha():Bool
+    {
+        return this.ordinal == 0;
+    }
+}

jascii/TermCanvas.hx
@@ -83,12 +83,12 @@ class TermCanvas implements jascii.display.ISurface
         return cached;
     }
 
-    public function draw_char(x:Int, y:Int, ordinal:Int):Void
+    public function draw_char(x:Int, y:Int, sym:jascii.display.Symbol):Void
     {
-        var cached:Int = this.font_cache.get(ordinal);
+        var cached:Int = this.font_cache.get(sym.ordinal);
 
         if (cached == null)
-            cached = this.add_to_font_cache(ordinal);
+            cached = this.add_to_font_cache(sym.ordinal);
 
         this.ctx.drawImage(this.font_canvas,
             cached, 0, TermFont.WIDTH, TermFont.HEIGHT,

jascii/display/Animation.hx
@@ -10,7 +10,7 @@ typedef AnimOptions = {
 
 class Animation extends Sprite
 {
-    private var frames:Array<Array<Array<Int>>>;
+    private var frames:Array<Array<Array<Symbol>>>;
     private var frame_options:Array<AnimOptions>;
     private var current:Int;
 
@@ -19,14 +19,15 @@ class Animation extends Sprite
 
     public var loopback:Bool;
 
-    public function new(?frames = null, ?options:Array<AnimOptions>)
+    public function new( ?frames:Array<Array<Array<Int>>> = null
+                       , ?options:Array<AnimOptions>)
     {
         super();
 
         if (frames == null)
             frames = new Array();
 
-        this.frames = frames;
+        this.frames = Lambda.array(Lambda.map(frames, this.generate_frame));
         this.frame_options = options;
         this.current = 0;
 
@@ -38,6 +39,23 @@ class Animation extends Sprite
         this.grow_sprite();
     }
 
+    private inline function
+        generate_frame(frame:Array<Array<Int>>):Array<Array<Symbol>>
+    {
+        var out:Array<Array<Symbol>> = new Array();
+
+        for (row in frame) {
+            var out_row:Array<Symbol> = new Array();
+
+            for (col in row)
+                out_row.push(new Symbol(col));
+
+            out.push(out_row);
+        }
+
+        return out;
+    }
+
     private inline function grow_sprite():Void
     {
         var width:Int = 0;
@@ -46,7 +64,7 @@ class Animation extends Sprite
         for (frame in this.frames) {
             var new_width = Lambda.fold(
                 frame,
-                function(row:Array<Int>, acc:Int)
+                function(row:Array<Symbol>, acc:Int)
                     return row.length > acc ? row.length : acc,
                 0
             );
@@ -62,7 +80,8 @@ class Animation extends Sprite
             this.height = height;
     }
 
-    public inline function add_frame(frame:Array<Array<Int>>):Array<Array<Int>>
+    public inline function
+        add_frame(frame:Array<Array<Symbol>>):Array<Array<Symbol>>
     {
         this.frames.push(frame);
         this.grow_sprite();

jascii/display/ISurface.hx
@@ -2,7 +2,7 @@ package jascii.display;
 
 interface ISurface
 {
-    public function draw_char(x:Int, y:Int, char:Int):Void;
+    public function draw_char(x:Int, y:Int, sym:Symbol):Void;
     // XXX: These should be handled by events (such as resize).
     public var width:Int;
     public var height:Int;

jascii/display/Sprite.hx
@@ -2,7 +2,7 @@ package jascii.display;
 
 class Sprite extends Object
 {
-    public var ascii(default, null):Array<Array<Int>>;
+    public var ascii(default, null):Array<Array<Symbol>>;
     public var rect(default, null):Rect;
 
     public function new()
@@ -32,7 +32,7 @@ class Sprite extends Object
         return super.set_surface(val);
     }
 
-    public function blit(ascii:Array<Array<Int>>):Void
+    public function blit(ascii:Array<Array<Symbol>>):Void
     {
         this.ascii = ascii;
 

jascii/display/Surface.hx
@@ -35,7 +35,7 @@ class Surface extends Object
     {
         for (y in rect.y...(rect.y + rect.height))
             for (x in rect.x...(rect.x + rect.width))
-                this.draw_char(x, y, char);
+                this.draw_char(x, y, new Symbol(char));
     }
 
     public function redraw_rect(rect:Rect):Void
@@ -47,10 +47,10 @@ class Surface extends Object
                 this.blit(sprite);
     }
 
-    private inline function draw_char(x:Int, y:Int, ordinal:Int):Void
+    private inline function draw_char(x:Int, y:Int, sym:Symbol):Void
     {
         if (x <= this.width && y <= this.height)
-            this.provider.draw_char(x, y, ordinal);
+            this.provider.draw_char(x, y, sym);
     }
 
     private function blit(sprite:Sprite):Void
@@ -60,7 +60,7 @@ class Surface extends Object
 
         for (yi in 0...sprite.ascii.length) {
             for (xi in 0...sprite.ascii[yi].length) {
-                if (sprite.ascii[yi][xi] == 0)
+                if (sprite.ascii[yi][xi].is_alpha())
                     continue;
 
                 this.draw_char(

jascii/test/TestCanvas.hx
@@ -5,7 +5,7 @@ class TestCanvas implements jascii.display.ISurface
     public var width:Int;
     public var height:Int;
 
-    private var area:Array<Int>;
+    private var area:Array<jascii.display.Symbol>;
 
     public function new()
     {
@@ -15,12 +15,12 @@ class TestCanvas implements jascii.display.ISurface
         this.area = new Array();
 
         for (i in 0...(this.width * this.height))
-            this.area.push(" ".code);
+            this.area.push(new jascii.display.Symbol(" ".code));
     }
 
-    public function draw_char(x:Int, y:Int, ordinal:Int):Void
+    public function draw_char(x:Int, y:Int, sym:jascii.display.Symbol):Void
     {
-        this.area[y * this.width + x] = ordinal;
+        this.area[y * this.width + x] = sym;
     }
 
     public function extract(x:Int, y:Int, width:Int, height:Int):Array<String>
@@ -43,7 +43,7 @@ class TestCanvas implements jascii.display.ISurface
             if (cur_y < y || cur_y >= y + height)
                 continue;
 
-            buf += String.fromCharCode(this.area[c]);
+            buf += String.fromCharCode(this.area[c].ordinal);
         }
 
         return out;
@@ -59,7 +59,7 @@ class TestCanvas implements jascii.display.ISurface
             if (c > 0 && c % this.width == 0)
                 out += "|\n|";
 
-            out += String.fromCharCode(this.area[c]);
+            out += String.fromCharCode(this.area[c].ordinal);
         }
 
         out += "|\n`" + StringTools.rpad("", "-", this.width) + "'";

commit f3e606335e631a4f1963f50426a4d028a4c77926

Symbol: Rewrite as an abstract type.
With this we finally don't have any cruft in the output JS file anymore and
after some benchmarking I discovered that we now finally don't suffer from the
regression introduced in f323bfb anymore :-)

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

jascii/display/Symbol.hx
@@ -1,25 +1,32 @@
 package jascii.display;
 
-class Symbol
+abstract Symbol(Int)
 {
-    public var ordinal(default, null):Int;
-    public var fgcolor(default, null):Int;
-    public var bgcolor(default, null):Int;
+    public var ordinal(get, set):Int;
+    public var fgcolor(get, set):Int;
+    public var bgcolor(get, set):Int;
 
     public inline function new(ord:Int, fg:Int = 0, bg:Int = 0)
-    {
-        this.ordinal = ord;
-        this.fgcolor = fg;
-        this.bgcolor = bg;
-    }
+        this = bg << 16 | fg << 8 | ord;
 
-    public inline function get_hash():Int
-    {
-        return this.ordinal << 16 | this.fgcolor << 8 | this.bgcolor;
-    }
+    private inline function get_ordinal():Int
+        return this & 0xff;
+
+    private inline function set_ordinal(ord:Int):Int
+        return this |= ord;
+
+    private inline function get_fgcolor():Int
+        return (this & 0xff00) >> 8;
+
+    private inline function set_fgcolor(fg:Int):Int
+        return this |= (fg << 8);
+
+    private inline function get_bgcolor():Int
+        return (this & 0xff0000) >> 16;
+
+    private inline function set_bgcolor(bg:Int):Int
+        return this |= (bg << 16);
 
     public inline function is_alpha():Bool
-    {
-        return this.ordinal == 0;
-    }
+        return this & 0xff == 0;
 }
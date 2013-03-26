f3e606335e631a4f1963f50426a4d028a4c77926
aszlig <aszlig@redmoonstudios.org>
2013-03-26 21:40:16 +0100

    Symbol: Rewrite as an abstract type.
    With this we finally don't have any cruft in the output JS file anymore and
    after some benchmarking I discovered that we now finally don't suffer from the
    regression introduced in f323bfb anymore :-)
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- jascii/display/Symbol.hx
+++ jascii/display/Symbol.hx
@@ -1,25 +1,32 @@
 package jascii.display;
 
-class Symbol
+abstract Symbol(Int)
 {
-    public var ordinal(default, null):Int;
-    public var fgcolor(default, null):Int;
-    public var bgcolor(default, null):Int;
+    public var ordinal(get, set):Int;
+    public var fgcolor(get, set):Int;
+    public var bgcolor(get, set):Int;
 
     public inline function new(ord:Int, fg:Int = 0, bg:Int = 0)
-    {
-        this.ordinal = ord;
-        this.fgcolor = fg;
-        this.bgcolor = bg;
-    }
+        this = bg << 16 | fg << 8 | ord;
 
-    public inline function get_hash():Int
-    {
-        return this.ordinal << 16 | this.fgcolor << 8 | this.bgcolor;
-    }
+    private inline function get_ordinal():Int
+        return this & 0xff;
+
+    private inline function set_ordinal(ord:Int):Int
+        return this |= ord;
+
+    private inline function get_fgcolor():Int
+        return (this & 0xff00) >> 8;
+
+    private inline function set_fgcolor(fg:Int):Int
+        return this |= (fg << 8);
+
+    private inline function get_bgcolor():Int
+        return (this & 0xff0000) >> 16;
+
+    private inline function set_bgcolor(bg:Int):Int
+        return this |= (bg << 16);
 
     public inline function is_alpha():Bool
-    {
-        return this.ordinal == 0;
-    }
+        return this & 0xff == 0;
 }
