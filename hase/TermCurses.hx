e06292b474b9bdffc51c53cf51f874070c367cbd
aszlig <aszlig@redmoonstudios.org>
2013-06-03 14:59:28 +0200

    termcurses: Accumulate escapes into a StringBuf.
    That way we can avoid unnecessary syscalls of write() and it more or less feels
    like "transactions".
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- hase/TermCurses.hx
+++ hase/TermCurses.hx
@@ -43,6 +43,7 @@ typedef TermSize = {
     public var height:Int;
 
     private var output:haxe.io.Output;
+    private var buffer:StringBuf;
 
     private var last_x:Int;
     private var last_y:Int;
@@ -55,18 +56,27 @@ typedef TermSize = {
         this.height = ts.height;
 
         this.output = Sys.stdout();
-        this.write_csi("2J");
 
         this.install_cleanup();
-        this.write_csi("?25l");
+        this.init_term();
 
-        this.write_csi("0;0f");
         this.last_x = this.last_y = 0;
+    }
+
+    private inline function init_term():Void
+    {
+        this.begin_op();
+
+        this.buffer.add("\x1b7");
+        this.write_csi("2J");
+        this.write_csi("?25l");
+        this.write_csi("0;0f");
+
+        this.flush_op();
     }
 
     private inline function install_cleanup():Void
     {
-        this.output.writeString("\x1b7");
 
         untyped __cpp__("
             if (signal(SIGINT, SIG_IGN) != SIG_IGN)
@@ -91,22 +101,35 @@ typedef TermSize = {
         };
     }
 
+    private function begin_op():Void
+    {
+        this.buffer = new StringBuf();
+    }
+
+    private function flush_op():Void
+    {
+        this.output.writeString(this.buffer.toString());
+    }
+
     private function write_csi(sequence:String):Void
     {
-        this.output.writeInt8(27);
-        this.output.writeString("[" + sequence);
+        this.buffer.add("\x1b[" + sequence);
     }
 
     public function draw_char(x:Int, y:Int, sym:hase.display.Symbol):Void
     {
+        this.begin_op();
+
         if (this.last_y != y || this.last_x != x)
             this.write_csi('${y};${x}f');
 
         this.write_csi('38;5;${sym.fgcolor};48;5;${sym.bgcolor}m');
-        this.output.writeInt8(sym.ordinal);
+        this.buffer.addChar(sym.ordinal);
         this.write_csi("m");
 
         this.last_x = x >= this.width ? x : x + 1;
         this.last_y = y;
+
+        this.flush_op();
     }
 }
