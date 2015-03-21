473b46eaaf3551b60c0782b3dacf7bc34223112d
aszlig <aszlig@redmoonstudios.org>
2015-03-21 04:23:05 +0100

    Add default.nix to build the example/run tests.
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

---
+++ default.nix
@@ -0,0 +1,30 @@
+with import <nixpkgs> {};
+
+let
+  isAllowed = path: type: with lib; let
+    relative = removePrefix "/" (removePrefix (toString ./.) (toString path));
+    splitted = splitString "/" relative;
+    comp1 = head splitted;
+    allowed = comp1 == "example" || comp1 == "hase" || comp1 == "test.hxml";
+  in splitted != [] && allowed;
+
+in stdenv.mkDerivation {
+  name = "hase-example";
+  src = builtins.filterSource isAllowed ./.;
+  buildInputs = [ haxe hxcpp neko ];
+
+  buildPhase = ''
+    (cd example && haxe example.hxml)
+  '';
+
+  doCheck = true;
+  checkPhase = ''
+    haxe test.hxml
+  '';
+
+  installPhase = ''
+    install -vD example/build/Example "$out/bin/hase-example"
+    install -m 0644 -vD example/example.js "$out/share/hase/example.js"
+    install -m 0644 -vD example/example.html "$out/share/hase/example.html"
+  '';
+}
