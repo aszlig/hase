ce6919bef2d764f98a00c5b58a04365817332637
aszlig <aszlig@redmoonstudios.org>
2015-03-26 00:19:59 +0100

    tests: Alphobetically sort testrunner definition.
    Just cosmetics, I just couldn't stand it ;-)
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- hase/test/Main.hx
+++ hase/test/Main.hx
@@ -25,16 +25,16 @@ class Main
     public static function main():Void
     {
         var runner = new haxe.unit.TestRunner();
+        runner.add(new hase.test.cases.AnimationParserTest());
+        runner.add(new hase.test.cases.AnimationTest());
+        runner.add(new hase.test.cases.ColorTableTest());
+        runner.add(new hase.test.cases.FrameAreaParserTest());
+        runner.add(new hase.test.cases.MatrixTest());
+        runner.add(new hase.test.cases.MotionTest());
+        runner.add(new hase.test.cases.PVectorTest());
+        runner.add(new hase.test.cases.RectTest());
         runner.add(new hase.test.cases.SpriteTest());
         runner.add(new hase.test.cases.SurfaceTest());
-        runner.add(new hase.test.cases.AnimationTest());
-        runner.add(new hase.test.cases.MatrixTest());
-        runner.add(new hase.test.cases.AnimationParserTest());
-        runner.add(new hase.test.cases.FrameAreaParserTest());
-        runner.add(new hase.test.cases.ColorTableTest());
-        runner.add(new hase.test.cases.RectTest());
-        runner.add(new hase.test.cases.PVectorTest());
-        runner.add(new hase.test.cases.MotionTest());
         runner.run();
     }
 }
