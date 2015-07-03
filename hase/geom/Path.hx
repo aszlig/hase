8804a882e4aa49f21472fd633f96320b0ee00fe5
aszlig <aszlig@redmoonstudios.org>
2015-07-03 22:46:33 +0200

    geom.Path: Turn Bresenham class into a function.
    We really don't need a class for the Bresenham's line algorithm and thus
    also don't need to abuse end values by setting them nullable in order to
    prevent using of a boolean flag or other weird workarounds.
    
    In the end we don't even expose the algorithm to users of the API,
    because if we want to draw a line we can still use a Path with two
    nodes.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- hase/geom/Path.hx
+++ hase/geom/Path.hx
@@ -22,86 +22,48 @@ package hase.geom;
 
 using hase.utils.Misc;
 
-typedef Point = {
-    var x:Int;
-    var y:Int;
-};
-
-private class Bresenham
-{
-    private var end_x:Null<Int>;
-    private var end_y:Null<Int>;
-
-    private var diff_x:Int;
-    private var diff_y:Int;
-
-    private var error:Int;
-
-    private var x:Int;
-    private var y:Int;
-
-    public function new(x0:Int, x1:Int, y0:Int, y1:Int)
-    {
-        this.end_x = x1;
-        this.end_y = y1;
-
-        this.diff_x = (x0 - this.end_x).intabs();
-        this.diff_y = -(y0 - this.end_y).intabs();
-
-        this.error = this.diff_x + this.diff_y;
-
-        this.x = x0;
-        this.y = y0;
-    }
-
-    public function hasNext():Bool
-        return this.end_x != null && this.end_y != null;
-
-    public function next():Point
-    {
-        var result:Point = {
-            x: this.x,
-            y: this.y
-        };
-
-        if (this.x == this.end_x && this.y == this.end_y) {
-            this.end_x = this.end_y = null;
-            return result;
-        }
-
-        var eshift:Int = this.error << 1;
-
-        if (eshift < this.diff_x) {
-            this.error += this.diff_x;
-            this.y += this.y.sigcmp(this.end_y);
-        }
-
-        if (eshift > this.diff_y) {
-            this.error += this.diff_y;
-            this.x += this.x.sigcmp(this.end_x);
-        }
-
-        return result;
-    }
-}
-
 abstract Path (Array<PVector>)
 {
     public inline function new(path:Array<PVector>)
         this = path;
 
-    private inline function
-        draw_pixel_line( from:PVector, to:PVector
-                       , f:Int -> Int -> Void, is_cont:Bool = false
-                       ):Void
+    private function bresenham( from:PVector, to:PVector
+                              , f:Int -> Int -> Void
+                              , is_cont:Bool = false
+                              ):Void
     {
-        var b = new Bresenham(
-            Math.round(from.x), Math.round(to.x),
-            Math.round(from.y), Math.round(to.y)
-        );
+        var x:Int = Math.round(from.x);
+        var y:Int = Math.round(from.y);
 
-        if (is_cont) b.next();
-        for (p in b) f(p.x, p.y);
+        var end_x:Int = Math.round(to.x);
+        var end_y:Int = Math.round(to.y);
+
+        var diff_x:Int = (x - end_x).intabs();
+        var diff_y:Int = -(y - end_y).intabs();
+
+        var error:Int = diff_x + diff_y;
+
+        while (true) {
+            if (is_cont)
+                is_cont = false;
+            else
+                f(x, y);
+
+            if (x == end_x && y == end_y)
+                break;
+
+            var eshift:Int = error << 1;
+
+            if (eshift < diff_x) {
+                error += diff_x;
+                y += y.sigcmp(end_y);
+            }
+
+            if (eshift > diff_y) {
+                error += diff_y;
+                x += x.sigcmp(end_x);
+            }
+        }
     }
 
     public inline function
@@ -111,7 +73,7 @@ abstract Path (Array<PVector>)
             var x:Int = Std.int(this[0].x);
             var y:Int = Std.int(this[0].y);
             m.set(x, y, f(x, y, m.get(x, y)));
-        } else for (i in 0...(this.length - 1)) Path.draw_pixel_line(
+        } else for (i in 0...(this.length - 1)) Path.bresenham(
             this[i], this[i + 1],
             inline function(x:Int, y:Int) {
                 m.set(x, y, f(x, y, m.get(x, y)));
