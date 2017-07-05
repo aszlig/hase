11e3f323d27f49f12f0b352cf807bb9021f0cea8
aszlig <aszlig@redmoonstudios.org>
2017-07-05 23:14:31 +0200

    Update copyright years
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- hase/test/cases/MotionTest.hx
+++ hase/test/cases/MotionTest.hx
@@ -1,4 +1,4 @@
-/* Copyright (C) 2015 aszlig
+/* Copyright (C) 2015-2017 aszlig
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
