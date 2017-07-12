b7a1ecb1f732558835b958bc2b8bc52d19114e8e
aszlig <aszlig@redmoonstudios.org>
2017-07-12 23:40:14 +0200

    canvas: Add code to show redraw of dirty rects
    Debugging performance issues is a lot easier when you actually see
    what's going on, so when compiling with the debug flag, every area that
    gets redrawn in the canvas now gets a colored rectangle around the whole
    area.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- hase/term/Canvas.hx
+++ hase/term/Canvas.hx
@@ -26,6 +26,29 @@ import hase.geom.Rect;
 
 @:require(js) class Canvas implements Interface
 {
+    #if debug
+    // From https://goo.gl/mhxhqY minus dark colors:
+    private static var DISTINCT_COLORS:Array<String> = [
+        "#ffff00", "#1ce6ff", "#ff34ff", "#ff4a46", "#008941", "#006fa6",
+        "#a30059", "#ffdbe5", "#7a4900", "#63ffac", "#b79762", "#004d43",
+        "#8fb0ff", "#997d87", "#5a0007", "#809693", "#feffe6", "#1b4400",
+        "#4fc601", "#3b5dff", "#4a3b53", "#ff2f80", "#61615a", "#ba0900",
+        "#6b7900", "#00c2a0", "#ffaa92", "#ff90c9", "#b903aa", "#d16100",
+        "#ddefff", "#7b4f4b", "#a1c299", "#300018", "#0aa6d8", "#013349",
+        "#00846f", "#372101", "#ffb500", "#c2ffed", "#a079bf", "#cc0744",
+        "#c0b9b2", "#c2ff99", "#001e09", "#00489c", "#6f0062", "#0cbd66",
+        "#eec3ff", "#456d75", "#b77b68", "#7a87a1", "#788d66", "#885578",
+        "#fad09f", "#ff8a9a", "#d157a0", "#bec459", "#456648", "#0086ed",
+        "#886f4c", "#34362d", "#b4a8bd", "#00a6aa", "#452c2c", "#636375",
+        "#a3c8c9", "#ff913f", "#938a81", "#575329", "#00fecf", "#b05b6f",
+        "#8cd0ff", "#3b9700", "#04f757", "#c8a1a1", "#1e6e00", "#7900d7",
+        "#a77500", "#6367a9", "#a05837", "#6b002c", "#772600", "#d790ff",
+        "#9b9700", "#549e79", "#fff69f", "#201625", "#72418f", "#bc23ff",
+        "#99adc0", "#3a2465", "#922329", "#5b4534", "#fde8dc", "#404e55",
+        "#0089a3", "#cb7e98", "#a4e804", "#324e72", "#6a3a4c",
+    ];
+    #end
+
     private var canvas:js.html.CanvasElement;
     private var ctx:js.html.CanvasRenderingContext2D;
 
@@ -77,22 +100,22 @@ import hase.geom.Rect;
 
     private inline function cursor2x(x:Int):Int
     {
-#if debug
+        #if debug
         if (x * Font.WIDTH > this.canvas.width)
             trace("X cursor value of " + x + " exceeds width of " +
                   this.canvas.width + " pixels!");
-#end
+        #end
 
         return x * Font.WIDTH;
     }
 
     private inline function cursor2y(y:Int):Int
     {
-#if debug
+        #if debug
         if (y * Font.HEIGHT > this.canvas.height)
             trace("Y cursor value of " + y + " exceeds height of " +
                   this.canvas.height + " pixels!");
-#end
+        #end
 
         return y * Font.HEIGHT;
     }
@@ -153,5 +176,16 @@ import hase.geom.Rect;
                 this.cursor2x(rect.x + x), this.cursor2y(rect.y + y),
                 Font.WIDTH, Font.HEIGHT);
         });
+
+        #if debug
+        var color:Int = Std.random(Canvas.DISTINCT_COLORS.length);
+        this.ctx.strokeStyle = Canvas.DISTINCT_COLORS[color];
+        this.ctx.strokeRect(
+            this.cursor2x(rect.x),
+            this.cursor2y(rect.y),
+            area.width * Font.WIDTH,
+            area.height * Font.HEIGHT
+        );
+        #end
     }
 }
